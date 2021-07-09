999cf44e9c227dcd83214ab1e0db4b7f3d1573d5
Alireza Mortezaei <mortezaei_alireza@yahoo.com>
2021-07-09 13:33:39 +0430

    get_object_40 4 used to fix a bug

--- first/views.py
+++ first/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Article
 
@@ -16,7 +16,7 @@ def home(request):
 
 
 def detail(request, slug):
-    article = Article.objects.get(slug=slug)
+    article = get_object_or_404(Article, slug=slug)
 
     context = {
         'article': article
